398234ddc4a6413b4b314806f111679136fadcae
duanhz <767763856@qq.com>
2017-06-30 15:16:35 +0800

    建库建表

---
+++ src/main/sql/db.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE crosslog CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+
+FLUSH PRIVILEGES;
---
+++ src/main/sql/table.log.sql
@@ -0,0 +1,14 @@
+/*==============================================================*/
+/* Table: access_log                                             */
+/*==============================================================*/
+create table access_log
+(
+   id                   int(9)                         auto_increment not null,
+   request_id              varchar(10)                       null,
+   server_id              varchar(100)                       null,
+   client_id              varchar(100)                       null,
+   req_json              varchar(500)                        null,
+   resp_json              varchar(1000)                           null,
+   cost_time_mills        int(9)                              null,
+   primary key (id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
